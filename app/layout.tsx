cd5c24c2a0c512264892ccb505d6be15936556ee
Stephen Lin <mstephenlin@gmail.com>
2024-02-15 09:19:00 -0800

    fix: remove metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,6 @@ export const metadata: Metadata = {
   title: "Outcastral Labs",
   description: "Outcastral Labs is a maker agency and capabilities shop that specializes applying AI software and robotics research to the industrial base",
   keywords: "AI, artificial intelligence, industrial manufacturing, industrial, manufacturing, robotics, edge computing, software, hardware, IoT",
-  image: "https://outcastral.com/_next/static/media/hoodLogo.a25a42f3.png"
 };
 
 export default function RootLayout({
