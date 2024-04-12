258122a6954c28d1a315231eed806feb28fd30f8
Stephen Lin <mstephenlin@gmail.com>
2024-04-11 17:15:29 -0700

    fix: change responsive sizing for img

--- app/page.tsx
+++ app/page.tsx
@@ -94,7 +94,7 @@ export default function Home() {
               priority={true}
               quality={10}
               fill
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 45vw, 33vw"
+              sizes="(max-width: 768px) 35vw, (max-width: 1200px) 45vw, 25vw"
               style={{
                 objectFit: "contain",
                 pointerEvents: "none",
